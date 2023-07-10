c67b0c01f192479f88869a587b2c8b6a672c9739
Georgii174 <116789457+Georgii174@users.noreply.github.com>
2023-07-11 00:01:28 +0500

    fix body

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/users.js');
 const cardRouter = require('./routes/cards.js');
-const NotFoundError = require('./errors/NotFoundError.js');
-const messages = require('./errors/const.js');
+const { NotFoundError } = require('./errors/NotFoundError.js');
+const { messages } = require('./errors/const.js');
 const errors = require('./errors/index.js')
 const { PORT = 3000 } = process.env;
 
@@ -19,6 +19,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
 ;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    // _id: '64a96d28c028d5a5562a0556' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '64ac553b7753eda24a29d65d' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
 app.use('/users', userRouter.usersRoutes);
 app.use('/cards', cardRouter.cardsRoutes);
 // app.post('/signup', cardRouter.cardsRoutes);
@@ -26,13 +34,7 @@ app.use((req, res, next) => {
   next(new NotFoundError(messages.common.notFound));
 });
 app.use(errors.handleDefaultError);
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64a96d28c028d5a5562a0556' // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
 
-  next();
-});
 
 app.listen(PORT, () => {
     console.log(`Запуск сервера`);
