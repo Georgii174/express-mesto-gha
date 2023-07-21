9d4ccada97d6687c09c8ba7f323437df08513596
Georgii174 <116789457+Georgii174@users.noreply.github.com>
2023-07-21 15:22:00 +0500

    PR14 fix

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,13 +1,13 @@
-const { messages, statusCodes } = require('../errors/const.js');
+const { messages, statusCodes } = require('../errors/const');
 
 const errorMiddleware = (error, req, res, next) => {
-  const { statusCode = 500, message: message = ''} = error;
+  const { statusCode = 500, message = '' } = error;
 
   if (error.code === 11000) {
     res.status(statusCodes.conflict).send({ message: messages.user.conflictEmail });
     return;
   }
-  res.status(statusCode).send({ message: message });
+  res.status(statusCode).send({ message });
   next();
 };
 
